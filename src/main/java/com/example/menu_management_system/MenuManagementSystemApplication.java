80b5354b356b1ad8c4b355041e8c66dc40fe2c59
user <user@DESKTOP-2IHMMIK>
2026-05-26 15:09:50 +0300

    firstcom

---
+++ src/main/java/com/example/menu_management_system/MenuManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.menu_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/menu_management_system/MenuManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menu_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
