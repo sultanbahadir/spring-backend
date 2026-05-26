d038fc54088e36a74de55f3b73cf84e881dea35b
sila-B00 <bahadirs311@gmail.com>
2026-05-26 22:33:59 +0300

    MenuController metot harf uyumsuzluğu düzeltildi

--- src/main/java/controller/MenuController.java
+++ src/main/java/controller/MenuController.java
@@ -1,5 +1,32 @@
 package controller;
 
-public interface MenuController {
+import com.menu.dto.MenuDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import services.MenuServices;
 
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/menus")
+@CrossOrigin(origins = "*") 
+public class MenuController {
+
+    @Autowired
+    private MenuServices menuService;
+
+    @PostMapping("/saveOrUpdate")
+    public MenuDTO saveOrUpdate(@RequestBody MenuDTO request) {
+        return menuService.SaveOrUpdate(request); 
+    }
+
+    @GetMapping("/get")
+    public MenuDTO getByDate(@RequestParam LocalDate date) {
+        return menuService.getMenuByDate(date);
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public void deleteFood(@PathVariable Long id) {
+        menuService.deleteMenu(id);
+    }
 }
